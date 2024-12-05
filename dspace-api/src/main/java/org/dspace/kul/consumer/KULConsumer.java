4cc946f4d7263984666b567673b20b67131e79d8
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2024-12-05 17:48:38 +0100

    fixed consumer for adding policies to new bundle bitstreams

--- dspace-api/src/main/java/org/dspace/kul/consumer/KULConsumer.java
+++ dspace-api/src/main/java/org/dspace/kul/consumer/KULConsumer.java
@@ -1,5 +1,6 @@
 package org.dspace.kul.consumer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -15,6 +16,7 @@ import org.dspace.content.Bitstream;
 import org.dspace.content.Bundle;
 import org.dspace.content.Item;
 import org.dspace.content.factory.ContentServiceFactory;
+import org.dspace.content.service.BundleService;
 import org.dspace.content.service.ItemService;
 import org.dspace.core.Constants;
 import org.dspace.core.Context;
@@ -39,6 +41,7 @@ public class KULConsumer implements Consumer {
     protected ResourcePolicyService resourcePolicyService = AuthorizeServiceFactory.getInstance()
             .getResourcePolicyService();
     protected ItemService itemService = ContentServiceFactory.getInstance().getItemService();
+    protected BundleService bundleService = ContentServiceFactory.getInstance().getBundleService();
 
     private Set<QueuedItem> queue = new HashSet<>();
 
@@ -48,7 +51,12 @@ public class KULConsumer implements Consumer {
 
     @Override
     public void consume(Context ctx, Event event) throws Exception {
-        queue.add(new QueuedItem(event.getSubjectID(), event.getEventType()));
+        if (event.getEventType() == Event.ADD && event.getSubjectType() == Constants.BUNDLE) {
+            final Bundle bundle = bundleService.find(ctx, event.getSubjectID());
+            for (final Item item : bundle.getItems()) {
+                queue.add(new QueuedItem(item.getID(), Event.ADD));
+            }
+        }
     }
 
     @Override
@@ -59,17 +67,16 @@ public class KULConsumer implements Consumer {
         }
 
         for (final QueuedItem qi : queue) {
+            final Item item = itemService.find(ctx, qi.getItemId());
             if (qi.getEventType() == Event.ADD) {
-                final Item item = itemService.find(ctx, qi.getSubjectId());
-                final ResourcePolicy rp = resourcePolicyService.create(ctx);
-                rp.setAction(Constants.READ);
-                rp.setGroup(groupsMap.get(ADMINS_LOCAL_GROUP));
+                final List<ResourcePolicy> policies = new ArrayList<>();
+                policies.add(readForGroup(ctx, groupsMap.get(ADMINS_LOCAL_GROUP)));
                 for (final Bundle bundle : item.getBundles()) {
                     for (final Bitstream bitstream : bundle.getBitstreams()) {
                         for (final Group group : groupsMap.values()) {
                             authorizeService.removeGroupPolicies(ctx, bitstream, group);
                         }
-                        authorizeService.addPolicies(ctx, Arrays.asList(rp), bitstream);
+                        authorizeService.addPolicies(ctx, policies, bitstream);
                     }
                 }
             }
@@ -80,6 +87,13 @@ public class KULConsumer implements Consumer {
 
     @Override
     public void finish(Context ctx) throws Exception {
+    }
+
+    private ResourcePolicy readForGroup(final Context ctx, final Group group) throws Exception {
+        final ResourcePolicy rp = resourcePolicyService.create(ctx);
+        rp.setAction(Constants.READ);
+        rp.setGroup(group);
+        return rp;
     }
 
 }
--- dspace-api/src/main/java/org/dspace/kul/consumer/QueuedItem.java
+++ dspace-api/src/main/java/org/dspace/kul/consumer/QueuedItem.java
@@ -3,27 +3,19 @@ package org.dspace.kul.consumer;
 import java.util.UUID;
 
 public class QueuedItem {
-    private UUID subjectId;
+    private UUID itemId;
     private int eventType;
 
-    public QueuedItem(UUID subjectId, int eventType) {
-        this.subjectId = subjectId;
+    public QueuedItem(UUID itemId, int eventType) {
+        this.itemId = itemId;
         this.eventType = eventType;
     }
 
-    public UUID getSubjectId() {
-        return subjectId;
-    }
-
-    public void setSubjectId(UUID subjectId) {
-        this.subjectId = subjectId;
+    public UUID getItemId() {
+        return itemId;
     }
 
     public int getEventType() {
         return eventType;
-    }
-
-    public void setEventType(int eventType) {
-        this.eventType = eventType;
     }
 }
