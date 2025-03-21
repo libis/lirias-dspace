8b650b8410ac6f8fc81bb8088c0f22f208d00909
okaradeniz <oguzozgurkaradeniz@gmail.com>
2025-03-21 11:21:41 +0100

    mailing implementation wip

--- dspace-api/src/main/java/org/dspace/kul/consumer/Mailing.java
+++ dspace-api/src/main/java/org/dspace/kul/consumer/Mailing.java
@@ -1,6 +1,9 @@
 package org.dspace.kul.consumer;
 
 import org.dspace.content.Item;
+import org.dspace.content.MetadataValue;
+import org.dspace.core.Email;
+import org.dspace.core.I18nUtil;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -14,34 +17,73 @@ import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.logging.log4j.Logger;
-import org.dspace.content.MetadataValue;
 
 public class Mailing {
     private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(Mailing.class);
 
-    private final Services services = new Services();
+    private static final Services services = new Services();
 
     private static final String senderEmail = "";
     private static final String elementsCacheAPIUrl = "";
     private static final String elementsCacheUsername = "";
     private static final String elementsCachePassword = "";
 
-    public static void applyTo(final KULEvent event) {
+    public static void applyTo(final KULEvent event) throws Exception {
         // TODO
         if (event.isPhd()) {
+            Set<String> emailRecipients = getContributorEmails(event.getItem());
+            if (emailRecipients.isEmpty()) {
+                return;
+            }
             switch (event.getConsumeCaseEnum()) {
-                case REDEPOSIT:
-                case ADD_VIA_UI:
-                case DEPOSIT:
-                case REMOVE:
+                case REDEPOSIT: {
+                    Email email = Email
+                            .getEmail(I18nUtil.getEmailFilename(event.getCtx().getCurrentLocale(), "redeposit"));
+                    emailRecipients.forEach(r -> email.addRecipient(r));
+                    email.setReplyTo(senderEmail);
+                    email.send();
+                    break;
+                }
+                case ADD_VIA_UI: {
+                    Email email = Email.getEmail(
+                            I18nUtil.getEmailFilename(event.getCtx().getCurrentLocale(), "add_bitstream_via_ui"));
+                    emailRecipients.forEach(r -> email.addRecipient(r));
+                    email.setReplyTo(senderEmail);
+                    email.send();
+
+                    break;
+                }
+                case DEPOSIT: {
+                    Email email = Email
+                            .getEmail(I18nUtil.getEmailFilename(event.getCtx().getCurrentLocale(), "deposit"));
+                    emailRecipients.forEach(r -> email.addRecipient(r));
+                    email.setReplyTo(senderEmail);
+                    email.send();
+                    break;
+                }
+                case REMOVE: {
+                    Email email = Email
+                            .getEmail(I18nUtil.getEmailFilename(event.getCtx().getCurrentLocale(), "remove_bitstream"));
+                    emailRecipients.forEach(r -> email.addRecipient(r));
+                    email.setReplyTo(senderEmail);
+                    email.send();
+                    break;
+                }
                 case EDIT_PERMISSION: {
+                    Email email = Email.getEmail(
+                            I18nUtil.getEmailFilename(event.getCtx().getCurrentLocale(), "edit_bitstream_permission"));
+                    emailRecipients.forEach(r -> email.addRecipient(r));
+                    email.setReplyTo(senderEmail);
+                    email.send();
                     break;
                 }
                 default: {
                     log.error("phd mailing for this event not implemented: " + event.getConsumeCaseEnum().name());
                     break;
                 }
+
             }
+
         } else {
             switch (event.getConsumeCaseEnum()) {
                 case REDEPOSIT:
@@ -59,8 +101,7 @@ public class Mailing {
         }
     }
 
-
-    private Set<String> getContributorEmails(Item item) throws Exception{
+    private static Set<String> getContributorEmails(Item item) throws Exception {
         Set<String> uNumbers = new HashSet<>();
         for (String contributorType : List.of("author", "supervisor", "cosupervisor")) {
             services.itemService.getMetadata(item, "dc", "contributor", contributorType, Item.ANY).stream()
@@ -71,11 +112,13 @@ public class Mailing {
         for (String uNumber : uNumbers) {
             emails.add(getEmailAdress(uNumber));
         }
-        System.out.println(emails);
+        if (emails.isEmpty()) {
+            log.error("No email addresses found for item.");
+        }
         return emails;
     }
 
-    private String getUnumberFromMetadata(MetadataValue metadataValue) {
+    private static String getUnumberFromMetadata(MetadataValue metadataValue) {
         String[] splitMetadata = metadataValue.getValue().toString().split("\\;");
         if (splitMetadata != null && splitMetadata.length > 1) {
             return splitMetadata[1].trim();
@@ -84,7 +127,7 @@ public class Mailing {
         }
     }
 
-    private String getEmailAdress(String uNumber) throws Exception {
+    private static String getEmailAdress(String uNumber) throws Exception {
         String result;
         HttpGet request = new HttpGet(
                 elementsCacheAPIUrl + "email/user/" + uNumber);
