77abaa339b37c3f441d45dbff6d0e40f10475d75
okaradeniz <oguzozgurkaradeniz@gmail.com>
2025-07-09 13:49:40 +0200

    removed OA emails for Add via UI consumer

--- dspace-api/src/main/java/org/dspace/kul/consumer/Mailing.java
+++ dspace-api/src/main/java/org/dspace/kul/consumer/Mailing.java
@@ -288,20 +288,7 @@ public class Mailing {
             }
             case ADD_VIA_UI: {
                 System.out.println("OA emails: Add from UI case (no emails sent)");
-                // try {
-                //     if (event.getBitstream() != null) {
-                //         sendEmailAddViaUI(event, event.getBitstream());
-                //     } else {
-                //         for (Bitstream bitstream : event.getBitstreams()) {
-                //             sendEmailAddViaUI(event, bitstream);
-                //         }
-                //     }
-                //     break;
-                // } catch (Exception e) {
-                //     System.err.println("OA emails: " + e);
-                //     break;
-                // }
-
+                break;
             }
             case DEPOSIT: {
                 System.out.println("OA emails: Deposit case");
