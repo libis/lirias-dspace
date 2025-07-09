8d020b85ca0c4b9005c0099b8b0c263f752225e8
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2025-07-09 17:40:05 +0200

    code cleanup

--- dspace-api/src/main/java/org/dspace/kul/consumer/KULConsumer.java
+++ dspace-api/src/main/java/org/dspace/kul/consumer/KULConsumer.java
@@ -13,7 +13,6 @@ import org.apache.logging.log4j.Logger;
 import org.dspace.content.Bitstream;
 import org.dspace.content.Bundle;
 import org.dspace.content.Item;
-import org.dspace.content.MetadataValue;
 import org.dspace.core.Constants;
 import org.dspace.core.Context;
 import org.dspace.eperson.Group;
